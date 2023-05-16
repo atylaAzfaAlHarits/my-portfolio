4186d6e16b8b734b03118ad820bd6c814dadc6f4
atylaAzfaAlHarits <nimoonnations@gmail.com>
2023-05-16 10:45:51 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        // 'gradient-before': 'linear-gradient(to top, #1c0522, transparent)',
+        'darkness': ''
+      }
+    },
+  },
+  plugins: [],
+}
+
